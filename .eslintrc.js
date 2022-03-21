4622468142f3a7069e0817c33f3aaee5fd1b2046
dvynshgpt1 <CodewithDivyanshu@gmail.com>
2022-03-21 12:36:15 +0530

    prettier error resolved

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 0,
     'nuxt/no-cjs-in-config': 'off'
   }
 }
